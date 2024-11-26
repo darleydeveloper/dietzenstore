9643abfeb48b02768797ba1f58f96c1ab81068a7
darleydesarrollador <maquetasaristoteles@gmail.com>
2024-11-26 13:39:49 -0500

    Avance inicial para docker

---
+++ resources/views/partials/botonera.blade.php
@@ -0,0 +1,33 @@
+
+<nav class="px-6 py-4 bg-red-500 sm-flex sm:items-center h-20" >
+    <section class="flex justify-between bg-red">
+        <button class="text-gray-700 sm:hidden">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="current" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>    
+
+        <img class="w-12"src="https://flowbite.com/docs/images/logo.svg" alt="Logo DietzenStore">
+        
+        <div class="justify-between-end">
+            <button class="avatar placeholder btn btn-ghost btn-circle">
+                <h2>carrito</h2>
+            </button>
+            <button class="avatar online placeholder">
+                <div class="bg-neutral text-neutral-content w-12 rounded-full">
+                <a href="#" class="hover:underline"><span class="text-xl">AI</span></a>
+                </div>
+            </button>
+        </div>
+    </section>
+    <div class="flex flex-col items-center gap-2 mt-1 sm:flex-row sm:m-1">
+        <button class="text-gray-600 hover:bg-gray-200 px-2 w-full rounded">Niños</button>
+        <button class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded">Mujeres</button>
+        <button class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded">Hombres</button>
+        <button class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded">Corporativo</button>
+    </div>
+</nav>
+
+
+
---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,44 @@
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  
+
+  <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        </a>
+        <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+        </button>
+        <div class="navbar bg-base-100">
+          <div class="flex">
+            <div class="dropdown dropdown-end">
+              <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                <div class="indicator">
+                  <a href="#" class="hover:underline">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div class="avatar online placeholder">
+                <div class="bg-neutral text-neutral-content w-16 rounded-full">
+                  <a href="#" class="hover:underline"><span class="text-xl">AI</span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+</nav>
